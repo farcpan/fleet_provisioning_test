0c3ab496b90e18afd24f7a2cda3cb585d6451a1d
miyazaki <miyazaki.carp@gmail.com>
2026-09-07 05:26:16 +0900

    AppSync Event API のURLをOutputとして出力

--- src/lib/api-stack.ts
+++ src/lib/api-stack.ts
@@ -1,63 +1,75 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { AppSyncAuthorizationType, EventApi } from "aws-cdk-lib/aws-appsync";
 import { ContextParameters } from "../utils/context";
+
+interface ApiStackProps extends StackProps {
+  context: ContextParameters;
+}
 
 /**
  * API Stack
  * - for RestAPI
  * - for AppSync Events
  */
-interface ApiStackProps extends StackProps {
-  context: ContextParameters;
-}
-
 export class ApiStack extends Stack {
     public eventApi: EventApi;
 
     constructor(scope: Construct, id: string, props: ApiStackProps) {
-    super(scope, id, props);
+        super(scope, id, props);
 
-    const eventApiName = props.context.getResourceId("appsync-event-api")
-    this.eventApi = new EventApi(this, eventApiName, {
-        apiName: eventApiName,
-        authorizationConfig: {
-            authProviders: [
-                // ここにCognitoProviderも追加する @FIXME
-                {
-                    authorizationType: AppSyncAuthorizationType.IAM
-                }
-            ],
+        const eventApiName = props.context.getResourceId("appsync-event-api")
+        this.eventApi = new EventApi(this, eventApiName, {
+            apiName: eventApiName,
+            authorizationConfig: {
+                authProviders: [
+                    // ここにCognitoProviderも追加する @FIXME
+                    {
+                        authorizationType: AppSyncAuthorizationType.IAM
+                    }
+                ],
 
-            // @FIXME モバイル/WebアプリからのWebSocket接続
-            connectionAuthModeTypes: [
-                AppSyncAuthorizationType.IAM,
-                //AppSyncAuthorizationType.USER_POOL
-            ],
-            // @FIXME モバイル/WebアプリからのSubscribe
-            defaultSubscribeAuthModeTypes: [
-                AppSyncAuthorizationType.IAM,
-                //AppSyncAuthorizationType.USER_POOL
-            ],
+                // @FIXME モバイル/WebアプリからのWebSocket接続
+                connectionAuthModeTypes: [
+                    AppSyncAuthorizationType.IAM,
+                    //AppSyncAuthorizationType.USER_POOL
+                ],
+                // @FIXME モバイル/WebアプリからのSubscribe
+                defaultSubscribeAuthModeTypes: [
+                    AppSyncAuthorizationType.IAM,
+                    //AppSyncAuthorizationType.USER_POOL
+                ],
 
-            // サーバ側LambdaからのみPublish: IAM認証で処理
-            defaultPublishAuthModeTypes: [AppSyncAuthorizationType.IAM],
-        },
-    });
+                // サーバ側LambdaからのみPublish: IAM認証で処理
+                defaultPublishAuthModeTypes: [AppSyncAuthorizationType.IAM],
+            },
+        });
 
-    // Namespace(Pub/sub用のトピック)を追加
-    this.eventApi.addChannelNamespace(props.context.getResourceId("app-namespace"), {
-        channelNamespaceName: "app",
-        authorizationConfig: {
-            publishAuthModeTypes: [
-                AppSyncAuthorizationType.IAM,
-            ],
+        // Namespace(Pub/sub用のトピック)を追加
+        this.eventApi.addChannelNamespace(props.context.getResourceId("app-namespace"), {
+            channelNamespaceName: "app",
+            authorizationConfig: {
+                publishAuthModeTypes: [
+                    AppSyncAuthorizationType.IAM,
+                ],
 
-            // AppSync Event APIの認証方法と合わせる必要がある（将来的にCognito認証に変更） @FIXME
-            subscribeAuthModeTypes: [
-                AppSyncAuthorizationType.IAM,
-            ],
-        },
-    })
-  }
+                // AppSync Event APIの認証方法と合わせる必要がある（将来的にCognito認証に変更） @FIXME
+                subscribeAuthModeTypes: [
+                    AppSyncAuthorizationType.IAM,
+                ],
+            },
+        });
+
+        /////////////////////////////////////////////////////////////////////////////////
+        // AppSync EventのエンドポイントURL
+        /////////////////////////////////////////////////////////////////////////////////
+        const httpEndpoint = `https://${this.eventApi.httpDns}/event`;
+        const realtimeEndpoint =`wss://${this.eventApi.realtimeDns}/event/realtime`;
+        new CfnOutput(this, props.context.getResourceId("appsync-event-httpendpoint"), {
+            value: httpEndpoint
+        });
+        new CfnOutput(this, props.context.getResourceId("appsync-event-ws-endpoint"), {
+            value: realtimeEndpoint
+        });
+    }
 }
