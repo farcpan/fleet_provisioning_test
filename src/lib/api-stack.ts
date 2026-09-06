7aff478d637ac4ddaa09b5138fbf8753069c0b09
miyazaki <miyazaki.carp@gmail.com>
2026-09-06 11:51:48 +0900

    AppSync Event APIへのNamespace追加漏れ修正

--- src/lib/api-stack.ts
+++ src/lib/api-stack.ts
@@ -1,16 +1,6 @@
-import { Aws, Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { AppSyncAuthProvider, AppSyncAuthorizationType, EventApi } from "aws-cdk-lib/aws-appsync";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { join } from "path";
-import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
-import {
-  ManagedPolicy,
-  Role,
-  ServicePrincipal,
-  PolicyStatement
-} from "aws-cdk-lib/aws-iam";
+import { AppSyncAuthorizationType, EventApi } from "aws-cdk-lib/aws-appsync";
 import { ContextParameters } from "../utils/context";
 
 /**
@@ -28,26 +18,46 @@ export class ApiStack extends Stack {
     constructor(scope: Construct, id: string, props: ApiStackProps) {
     super(scope, id, props);
 
-
-    // IAM Provider
-    const iamProvider: AppSyncAuthProvider = { authorizationType: AppSyncAuthorizationType.IAM };
-    // @FIXME Cognito Provider そのうち追加
-
     const eventApiName = props.context.getResourceId("appsync-event-api")
     this.eventApi = new EventApi(this, eventApiName, {
         apiName: eventApiName,
         authorizationConfig: {
-            authProviders: [iamProvider],
+            authProviders: [
+                // ここにCognitoProviderも追加する @FIXME
+                {
+                    authorizationType: AppSyncAuthorizationType.IAM
+                }
+            ],
 
             // @FIXME モバイル/WebアプリからのWebSocket接続
-            // connectionAuthModeTypes: [AppSyncAuthorizationType.USER_POOL],
+            connectionAuthModeTypes: [
+                AppSyncAuthorizationType.IAM,
+                //AppSyncAuthorizationType.USER_POOL
+            ],
             // @FIXME モバイル/WebアプリからのSubscribe
-            // defaultSubscribeAuthModeTypes: [AppSyncAuthorizationType.USER_POOL],
+            defaultSubscribeAuthModeTypes: [
+                AppSyncAuthorizationType.IAM,
+                //AppSyncAuthorizationType.USER_POOL
+            ],
 
             // サーバ側LambdaからのみPublish: IAM認証で処理
             defaultPublishAuthModeTypes: [AppSyncAuthorizationType.IAM],
         },
     });
 
+    // Namespace(Pub/sub用のトピック)を追加
+    this.eventApi.addChannelNamespace(props.context.getResourceId("app-namespace"), {
+        channelNamespaceName: "app",
+        authorizationConfig: {
+            publishAuthModeTypes: [
+                AppSyncAuthorizationType.IAM,
+            ],
+
+            // AppSync Event APIの認証方法と合わせる必要がある（将来的にCognito認証に変更） @FIXME
+            subscribeAuthModeTypes: [
+                AppSyncAuthorizationType.IAM,
+            ],
+        },
+    })
   }
 }
